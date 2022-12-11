065ede5163065b3f1f91a0609a3c2e53645a5554
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-12-11 17:46:20 +0545

    level 5 error solved

--- Player.java
+++ Player.java
@@ -57,52 +57,90 @@ public class Player {
         }
         head = list.getNode(1);
         int streak = 1;
+        int position = 0;
+        int temp1 = 1, temp2=1, temp3=1, temp4=1;
+        int temp = 1;
+
         for (int i=0;head != null; i++) {
 //            System.oureturn this.score;t.println("Suit = " + rank);
 
 //            System.out.println("Before streak value: " + streak);
 //            System.out.println("IS head.next null? " + head.next);    OK
             if(head.next != null) {
-//                System.out.println("Rank sub: " + rank[i+1] + " -  " + rank[i] + " = "+ (rank[i + 1] - rank[i]));
+                System.out.println("Rank sub: " + rank[i+1] + " -  " + rank[i] + " = "+ (rank[i + 1] - rank[i]));
                 if ((rank[i + 1] - rank[i]) == 1) {
 //                    score++;
                     streak++;
+                    score = streak;
+                    System.out.println(score);
+
+                    // Lets try something new
+                    if (suit[i]%2 == suit[i+1]%2) {
+                        temp1++;
+
+                        if (suit[i] == suit[i+1]) {
+                            temp2++;
+                        }
+                    }
+                    if (temp1>temp3 || temp2>temp4) {
+                        temp3 = temp1;
+                        temp4 = temp2;
+                    }
+
 //                    System.out.println(streak + " position = " + i);
 //                    System.out.println("from 1: " + score);
                 } else {
-                    score = streak;
+                    if (streak > score) {
+                        position = i;
+                    }
                     streak = 1;
+                    temp1=1;
+                    temp2=1;
 
                 }
 
             }
-            if (score < streak) {
-                score = streak;
-            }
+
             head = head.next;
         }
+        if (score < streak) {
+            score = streak;
+        }
+        System.out.println("Position = " + position);
+        System.out.println("Streak: " + streak);
+        System.out.println("Score: " + score);
+
+        // If same color
+        if ((temp3==score)) {
+            // If same SUIT
+            if (temp4==score) {
+                score++;
+            }
+            score++;
+        }
+
         head = list.getNode(1);
-        int temp1 = 0, temp2=0, temp3=0;
-        for (int j=0; j<streak; j++) {
-            if (suit[j]%2 != 0) {
-                temp1++;
-
-            } else {
-                temp2++;
-
-            }
-            if (head.next != null) {
-                if (suit[j] == suit[j + 1]) {
-                    temp3++;
-                }
-            }
-        }
-        if ((temp1==streak || temp2==streak) && streak != 0) {
-            score++;
-        }
-        if ((temp3 == streak) && streak != 0) {
-            score++;
-        }
+//        int temp1 = 0, temp2=0, temp3=0;
+//        for (int j=position; j<score; j++) {
+//            if (suit[j]%2 != 0) {
+//                temp1++;
+//
+//            } else {
+//                temp2++;
+//
+//            }
+//            if (head.next != null) {
+//                if (suit[j] == suit[j + 1]) {
+//                    temp3++;
+//                }
+//            }
+//        }
+//        if ((temp1==streak || temp2==streak) && streak != 0) {
+//            score++;
+//        }
+//        if ((temp3 == streak) && streak != 0) {
+//            score++;
+//        }
     }
 
     @Override
